9e895696d5db7e0245825c4b098f971317a3f5cc
Hugo Mota <hugomotadev@gmail.com>
2024-01-24 17:02:48 -0300

    Update files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,14 @@ window.addEventListener("load", function(){
     loader.style.display = "none"
 
 })
+
+function abrirTab(idTab){
+    var conteudos = document.getElementsByClassName('tabs-conteudo')
+    
+    for (var cont = 0; cont < conteudos.length; cont++) {
+        console.log(i);
+
+    }
+
+    document.getElementById(idTab).style.display = 'block';
+}
