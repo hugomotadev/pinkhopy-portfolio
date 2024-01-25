2d899b57f6a58b88b70e4515fb5f640b9db54a1a
Hugo Mota <hugomotadev@gmail.com>
2024-01-25 16:51:07 -0300

    Update files

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,26 @@ window.addEventListener("load", function () {
 });
 
 // Clicando na tab-default - deixando texto ativo
-document.getElementById('tab-default').click();
+// document.getElementById('tab-default').click();
 
 
 // Função para midificar as classes das tab's na Section Experiências
 
-function abrirTab(event, idTab) {
-    var conteudos = document.getElementsByClassName("tabs-conteudo");
-    for (var cont = 0; cont < conteudos.length; cont++) {
-        conteudos[cont].style.display = "none";
-    }
+// function abrirTab(event, idTab) {
+//     var conteudos = document.getElementsByClassName("tabs-conteudo");
+//     for (var cont = 0; cont < conteudos.length; cont++) {
+//         conteudos[cont].style.display = "none";
+//     }
 
-    var tabs = document.getElementsByClassName("tab-button");
-    for (var cont = 0; cont < tabs.length; cont++) {
-        tabs[cont].className = tabs[cont].className.replace("ativo", "");
-    }
+//     var tabs = document.getElementsByClassName("tab-button");
+//     for (var cont = 0; cont < tabs.length; cont++) {
+//         tabs[cont].className = tabs[cont].className.replace("ativo", "");
+//     }
 
     
-    document.getElementById(idTab).style.display = "block";
-    event.currentTarget.className += " ativo";
-}
+//     document.getElementById(idTab).style.display = "block";
+//     event.currentTarget.className += " ativo";
+// }
+
+
 
